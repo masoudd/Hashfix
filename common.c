f8b385c939f5c786230f4478cfd1049cf2bf1a27
Masoud Naservand <relive.mn@gmail.com>
2017-01-10 22:35:17 +0330

    Moved a function to common.c
    Moved the function for computing human readable
    hashes to common.c

--- common.c
+++ common.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-void to_hex(int len, uint8_t *data, char *str) {
+void to_hex(int len, uint8_t data[static len], char str[static len * 2 + 1]) {
     for (int i = 0; i < len; i++)
         sprintf(str + 2 * i, "%02x", data[i]);
 
@@ -14,4 +14,15 @@ long fsize(FILE *file) {
     fseek(file, orig, SEEK_SET);
 
     return size;
+}
+
+int compute_hash_hex(size_t length, uint8_t data[static length], char hex[static MD5_DIGEST_SIZE]) {
+    struct md5_ctx ctx;
+    uint8_t digest[MD5_DIGEST_SIZE];
+
+    md5_init(&ctx);
+    md5_update(&ctx, length, data);
+    md5_digest(&ctx, MD5_DIGEST_SIZE, digest);
+    to_hex(MD5_DIGEST_SIZE, digest, hex);
+    return 0;
 }
--- common.h
+++ common.h
@@ -3,8 +3,10 @@
 
 #include <inttypes.h>
 #include <stdio.h>
+#include <nettle/md5.h>
 
-void to_hex(int, uint8_t *, char *);
+void to_hex(int len, uint8_t data[static len], char str[static len * 2 + 1]);
 long fsize(FILE *);
+int compute_hash_hex(size_t length, uint8_t data[static length], char hex[static MD5_DIGEST_SIZE]);
 
 #endif /* common_h */
